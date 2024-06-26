72b5877141ff2772c1be306b67529e3bc1223711
djzarka <yogevhadad24@gmail.com>
2024-06-26 19:30:13 +0300

    fix init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,13 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: 'AIzaSyCCqiURm_L8sG1djmtXdzylPa-wQf9KqBI',
-      appId: '1:580646667465:ios:df2aa35ba72e744c6df918',
-      messagingSenderId: '580646667465',
-      projectId: 'insta-moment',
-      storageBucket: 'insta-moment.appspot.com',
-
-    ),
+    options:  DefaultFirebaseOptions.currentPlatform,
+      // apiKey: 'AIzaSyCCqiURm_L8sG1djmtXdzylPa-wQf9KqBI',
+      // appId: '1:580646667465:ios:df2aa35ba72e744c6df918',
+      // messagingSenderId: '580646667465',
+      // projectId: 'insta-moment',
+      // storageBucket: 'insta-moment.appspot.com',
+    // ),
   );
   //   File imageFile = File('/Users/djzarka/AndroidStudioProjects/test2/assets/cover.jpeg');
   //   final fileName = '${DateTime.now().millisecondsSinceEpoch}.png';
